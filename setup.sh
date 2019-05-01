6050c554ed5f4cdae7ca74400b6565081ed12ac1
cormon1976 <kevin.cornally@gmail.com>
2019-05-01 20:38:50 +0000

    aded in saetup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+#  Setup Script for ChromeBook!!
+#
+# ZSH
+sudo apt-get install zsh -y
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+# SCREEN
+sudo apt-get  install screen
